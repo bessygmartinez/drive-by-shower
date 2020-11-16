b11b6fb6af22919fdd4b19f4de1772723edc9920
Bessy Gee <nakiwarai13@gmail.com>
2020-11-16 16:29:06 -0500

    added controllers for mongodb queries

---
+++ controllers/partiesController.js
@@ -0,0 +1,37 @@
+const db = require("../models");
+
+// Defining methods for the usersController
+module.exports = {
+  findAll: function(req, res) {
+    db.Parties
+      .find(req.query)
+      .sort({ "partyName": 1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Parties
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Parties
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Parties
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Parties
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
