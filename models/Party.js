f63ff533b525003bb3d4a97ff41849cd98327184
Bessy Gee <nakiwarai13@gmail.com>
2020-11-16 16:28:42 -0500

    added model for saving parties in mongodb

---
+++ models/Party.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+// Create Schema
+const PartiesSchema = new Schema({
+  partyName: {
+    type: String,
+    required: true
+  },
+  partySize: {
+    type: String,
+    required: true
+  },
+  partyAddress: {
+    type: String,
+    required: true
+  },
+  partyTime: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = Party = mongoose.model("parties", PartiesSchema);
---
+++ models/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    Parties: require("./Party")
+};
