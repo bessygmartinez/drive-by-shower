b318b7c8e6762a25faedf90874fe8f7a13851f02
Bessy Gee <nakiwarai13@gmail.com>
2020-11-21 18:31:35 -0500

    changed bullet points and spacing

--- client/src/components/Modal/index.js
+++ client/src/components/Modal/index.js
@@ -3,36 +3,35 @@ import Cookies from "js-cookie";
 import { Modal, Button } from "react-materialize";
 
 export class CovidModal extends Component {
-    constructor() {
-        super();
-        this.state = {
-          spanish: false
-        };
-      }
+  constructor() {
+    super();
+    this.state = {
+      spanish: false,
+    };
+  }
 
-    setCookie = () => {
-            Cookies.set('closeModal', 'true', { expires: 1 });
-          }
+  setCookie = () => {
+    Cookies.set("closeModal", "true", { expires: 1 });
+  };
 
-          handleToggle = (e) => {
-            e.preventDefault();
-        
-            this.setState({
-              spanish: !this.state.spanish,
-            });
-          };     
+  handleToggle = (e) => {
+    e.preventDefault();
 
-    render() {
-        let open = this.props.open
+    this.setState({
+      spanish: !this.state.spanish,
+    });
+  };
 
-        return (
-            <div>
-                <Modal
+  render() {
+    let open = this.props.open;
+
+    return (
+      <div>
+        <Modal
           actions={[
             <div style={{ float: "right" }} id="modal-close">
-              <Button modal="close" node="button" waves="teal"
-              onClick={this.setCookie}>
-              {this.state.spanish === false ? "Agree" : "De Acuerdo"}
+              <Button modal="close" node="button" waves="teal" onClick={this.setCookie}>
+                {this.state.spanish === false ? "Agree" : "De Acuerdo"}
               </Button>
             </div>,
             <div style={{ float: "left" }}>
@@ -62,62 +61,92 @@ export class CovidModal extends Component {
         >
           <p className={this.state.spanish === false ? "show-english" : "hide-english"}>
             We are committed to keeping ourselves and our loved ones safe. To help support the
-            health and safety of everyone, we would like to remind you of the following:</p>
+            health and safety of everyone, we would like to remind you of the following:
+          </p>
 
-            <br/>
-            <ul className={this.state.spanish === false ? "show-english" : "hide-english"}>
-              <li>- Wash your hands frequently</li>
-              <li>- Cover your cough or sneeze using your elbow</li>
-              <li>- Wear a mask!</li>
-              <li>
-                - Avoid close contact with others, such as shaking hands, touching, hugging, or
-                kissing.
-              </li>
-              <li>
-                - Please do not come if you experience fever, cough, or shortness of breath within
+          <ul className={this.state.spanish === false ? "show-english" : "hide-english"} style={{textAlign: "justify"}}>
+            <li className="tab">
+              • <span style={{ paddingLeft: "0.5em" }}>Wash your hands frequently.</span>
+            </li>
+            <li className="tab">
+              •{" "}
+              <span style={{ paddingLeft: "0.5em" }}>
+                Cover your cough or sneeze using your elbow.
+              </span>
+            </li>
+            <li className="tab">
+              • <span style={{ paddingLeft: "0.5em" }}>Wear a mask!</span>
+            </li>
+            <li className="tab">
+              • <span style={{ paddingLeft: "0.5em" }}>Avoid close contact with others, such as shaking hands, touching, hugging, or
+                kissing.</span>
+            </li>
+            <li className="tab">
+                • <span style={{ paddingLeft: "0.5em" }}>Please do not come if you experience fever, cough, or shortness of breath within
                 14 days of December 12th.
-              </li>
-              <li>
-                - Please do not come if you have had close contact with anyone infected with
+              </span>
+            </li>
+            <li className="tab">
+                • <span style={{ paddingLeft: "0.5em" }}>Please do not come if you have had close contact with anyone infected with
                 Covid-19, including individuals exhibiting Covid-19 symptoms, within 14 days of
                 December 12th.
-              </li>
-            </ul>
-            <br/>
+              </span>
+            </li>
+          </ul>
 
           <p className={this.state.spanish === false ? "show-english" : "hide-english"}>
             Thank you for your continued efforts in helping curb the spread of Covid-19.
           </p>
 
           <p className={this.state.spanish === true ? "show-spanish" : "hide-spanish"}>
-          Estamos comprometidos a nuestros seres queridos y a nosotros mismos. Para mantener
-          la salud y seguridad de todos, nos gustaría recordarle de lo siguiente:</p>
+            Estamos comprometidos a nuestros seres queridos y a nosotros mismos. Para mantener la
+            salud y seguridad de todos, nos gustaría recordarle de lo siguiente:
+          </p>
 
-          <br/>
-            <ul className={this.state.spanish === true ? "show-spanish" : "hide-spanish"}>
-              <li>- Lávese las manos con frecuencia</li>
-              <li>- Cúbrase al toser o estornudar con el codo</li>
-              <li>- ¡Usar una máscara!</li>
-              <li>
-                - Evite el contacto cercano con otras personas, como dar la mano, tocar, abrazar o besar.
-              </li>
-              <li>
-                - Por favor no venga si tiene fiebre, tos o dificultades respirando dentro de 14 días posteriores al 12 de Diciembre.
-              </li>
-              <li>
-                - Por favor no venga si ha tenido contacto cercano con alguien infectado con Covid-19, incluidas personas que presenten 
-                síntomas de Covid-19, dentro de 14 días posteriores al 12 de Diciembre.
-              </li>
-            </ul>
-            <br/>
-          
+          <ul className={this.state.spanish === true ? "show-spanish" : "hide-spanish"} style={{textAlign: "justify"}}>
+            <li className="tab">
+              • <span style={{ paddingLeft: "0.5em" }}>Lávese las manos con frecuencia.</span>
+            </li>
+            <li className="tab">
+              •{" "}
+              <span style={{ paddingLeft: "0.5em" }}>
+                Cúbrase al toser o estornudar con el codo.
+              </span>
+            </li>
+            <li className="tab">
+              • <span style={{ paddingLeft: "0.5em" }}>¡Usar una máscara!</span>
+            </li>
+            <li className="tab">
+              •{" "}
+              <span style={{ paddingLeft: "0.5em" }}>
+                Evite el contacto cercano con otras personas, como dar la mano, tocar, abrazar o
+                besar.
+              </span>
+            </li>
+            <li className="tab">
+              •{" "}
+              <span style={{ paddingLeft: "0.5em" }}>
+                Por favor no venga si tiene fiebre, tos o dificultades respirando dentro de 14 días
+                posteriores al 12 de Diciembre.
+              </span>
+            </li>
+            <li className="tab">
+              •{" "}
+              <span style={{ paddingLeft: "0.5em" }}>
+                Por favor no venga si ha tenido contacto cercano con alguien infectado con Covid-19,
+                incluidas personas que presenten síntomas de Covid-19, dentro de 14 días posteriores
+                al 12 de Diciembre.
+              </span>
+            </li>
+          </ul>
+
           <p className={this.state.spanish === true ? "show-spanish" : "hide-spanish"}>
-          Gracias por sus continuos esfuerzos en frenar la propagación del Covid-19.
+            Gracias por sus continuos esfuerzos en frenar la propagación del Covid-19.
           </p>
         </Modal>
-            </div>
-        )
-    }
+      </div>
+    );
+  }
 }
 
 export default CovidModal;
